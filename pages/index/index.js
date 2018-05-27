682da32606b7a95abd92ff5611d29400aa223422
ChinaForce_Charles <yanchi_zhang@163.com>
2018-05-27 22:13:47 +0100

    JSON
    read weather data from api request

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,16 @@
-Page({})
+Page({
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '西安市'
+      },
+      success: res => {
+       let result = res.data.result
+       let temp = result.now.temp
+       let weather = result.now.weather
+       console.log(temp,weather)
+      }
+    })
+  }
+})
