6925bb4596af42ff0e0d63374a3f5ea0e7e327f3
ChinaForce_Charles <yanchi_zhang@163.com>
2018-05-28 04:14:53 +0100

    combine data
    combine the fetched data with variable

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,22 @@
 Page({
+  data: {
+    nowTemp: '14°',
+    nowWeather: '多云'
+  },
   onLoad(){
     wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now',
+      url: 'https://test-miniprogram.com/api/weather/now', //API address
       data: {
-        city: '西安市'
+        city: '西安市' // name of city
       },
       success: res => {
        let result = res.data.result
        let temp = result.now.temp
        let weather = result.now.weather
-       console.log(temp,weather)
+       this.setData({
+         nowTemp: temp + '°',
+         nowWeather: weather
+       })
       }
     })
   }
