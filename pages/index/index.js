b5542fddd4e99bb439348f93d166eefcb378d25c
ChinaForce_Charles <yanchi_zhang@163.com>
2018-07-01 00:34:39 +0800

    refactor
    create setNow(result) and setHourlyWeather(result) to make the code
    more concise

--- pages/index/index.js
+++ pages/index/index.js
@@ -31,7 +31,7 @@ Page({
   onLoad() {
   this.getNow()
   },
-  getNow(ciallback){
+  getNow(callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now', //API address
       data: {
@@ -39,36 +39,41 @@ Page({
       },
       success: res => {
         let result = res.data.result
-        let temp = result.now.temp
-        let weather = result.now.weather
-        this.setData({
-          nowTemp: temp + '°',
-          nowWeather: weatherMap[weather],
-          nowWeatherBackground: '/images/' + weather + '-bg.png'
-        })
-        wx.setNavigationBarColor({
-          frontColor: '#000000',
-          backgroundColor: weatherColorMap[weather]
-        })
-
-        let forecast = result.forecast
-        let nowHour = new Date().getHours()
-        let hourlyWeather = []
-        for (let i = 0; i < 24; i += 3) {
-          hourlyWeather.push({
-            time: (i + nowHour) % 24 + '时',
-            iconPath: '/images/'+ forecast[i/3].weather +'-icon.png',
-            temp: forecast[i/3].temp+'°'
-          })
-        }
-        hourlyWeather[0].time = '现在'
-        this.setData({
-          forecast: hourlyWeather
-          })
+        this.setNow(result)
+        this.setHourlyWeather(result)
       },
       complete: ()=>{
         callback && callback()
       }
+    })
+  },
+  setNow(result){
+    let temp = result.now.temp
+    let weather = result.now.weather
+    this.setData({
+      nowTemp: temp + '°',
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png'
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather]
+    })
+  },
+  setHourlyWeather(result){
+    let forecast = result.forecast
+    let nowHour = new Date().getHours()
+    let hourlyWeather = []
+    for (let i = 0; i < 8; i += 1) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/images/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°'
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      forecast: hourlyWeather
     })
   }
 })
